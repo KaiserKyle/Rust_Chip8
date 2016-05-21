ad6cf8d17090c5abd8163dbddb9a18543994ebc3
KaiserKyle <KaiserKyle@gmail.com>
2016-05-20 22:11:55 -0700

    More opcodes implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-const chip8_fontset: [u8; 80] = [
+const CHIP8_FONTSET: [u8; 80] = [
   0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
   0x20, 0x60, 0x20, 0x20, 0x70, // 1
   0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -26,29 +26,42 @@ struct Chip8State {
     pc: usize,
     delay_timer: u16,
     sound_timer: u16,
-    stack_pointer: u16
+    stack_pointer: usize,
+    stack: Vec<usize>,
+    v: Vec<u16>
 }
 
 fn main() {
     // Initialize State
     let mut state: Chip8State = Default::default(); 
     let mut memory = vec![0u8; 4096];
-    let mut reg = vec![0u8; 16];
     let mut gfx = vec![0u8; 2048];
-    let mut stack = vec![0u16; 16];
-    let mut key_press = vec![0u8, 16];
+    let mut key_press = vec![0u8; 16];
     
     // Load fontset
     for x in 0..80 {
-        memory[x] = chip8_fontset[x];
+        memory[x] = CHIP8_FONTSET[x];
     }
     
     // Load program
     state.pc = 0x200;
     state.sound_timer = 0xF;
     state.delay_timer = 0x10;
+    state.stack = vec![0usize; 16];
+    state.v = vec![0u16; 16];
+    
+    // Test data
     memory[0x200] = 0xA2;
     memory[0x201] = 0xF0;
+    memory[0x202] = 0x22;
+    memory[0x203] = 0x08;
+    memory[0x208] = 0x31;
+    memory[0x209] = 0x01;
+    memory[0x20C] = 0x42;
+    memory[0x20D] = 0x01;
+    memory[0x210] = 0x12;
+    memory[0x211] = 0x00;
+    state.v[1] = 1;
     
     // Emulation loop
     loop {
@@ -65,7 +78,6 @@ fn emulate_cycle(state: &mut Chip8State, memory: &Vec<u8>) {
     
     execute_opcode(opcode, state);
     update_timers(state);
-    state.pc += 2;
     
     println!("{:?}", state);
     let _ = io::stdin().read_line(&mut line);
@@ -78,20 +90,45 @@ fn get_opcode(pc: usize, memory: &Vec<u8>) -> u16 {
 
 fn execute_opcode(opcode: u16, state: &mut Chip8State) {
     let decode = opcode & 0xF000;
-    match (decode) {
+    match decode {
+        0x1000 => {
+            println!("0x1 opcode");
+            state.pc = (opcode & 0x0FFF) as usize;
+            }
+        0x2000 => {
+            println!("0x2 opcode");
+            state.stack[state.stack_pointer] = state.pc;
+            state.stack_pointer += 1;
+            state.pc = (opcode & 0x0FFF) as usize;
+            }
+        0x3000 => {
+            println!("0x3 opcode");
+            if state.v[((opcode & 0x0F00) >> 8) as usize] == (opcode & 0x00FF) {
+                state.pc += 2;
+                }
+            state.pc += 2;
+            }
+        0x4000 => {
+            println!("0x4 opcode");
+            if state.v[((opcode & 0x0F00) >> 8) as usize] != (opcode & 0x00FF) {
+                state.pc += 2;
+                }
+            state.pc += 2;
+            }
         0xA000 => {
             println!("0xA opcode");
             state.index = opcode & 0x0FFF;
+            state.pc += 2;
             }
         _ => { println!("Unknown opcode: {:X}", decode)}
     }
 }
 
 fn update_timers(state: &mut Chip8State) {
-    if (state.delay_timer > 0) {
+    if state.delay_timer > 0 {
         state.delay_timer -= 1;
     }
-    if (state.sound_timer > 0) {
+    if state.sound_timer > 0 {
         state.sound_timer -= 1;
     }
 }
